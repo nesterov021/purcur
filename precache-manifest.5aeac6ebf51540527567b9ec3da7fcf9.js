02aa07ba6b70a8c9037c0acc1847385260ce2d40
nesterov021 <61201519+nesterov021@users.noreply.github.com>
2020-06-17 17:01:34 +0500

    Updates

---
+++ precache-manifest.5aeac6ebf51540527567b9ec3da7fcf9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4b4444ae169585d2d1df62b6333a10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa33120504f2e6b91a81",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d0cf6c0faba964300de7",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "fa33120504f2e6b91a81",
+    "url": "./static/js/2.694dca1e.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.694dca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0cf6c0faba964300de7",
+    "url": "./static/js/main.5a7e782c.chunk.js"
+  },
+  {
+    "revision": "cfe92b999198c276e34d",
+    "url": "./static/js/runtime-main.1e1120dc.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0945967b2bd7bc096278140fa29f50a6.js"
+  "./precache-manifest.5aeac6ebf51540527567b9ec3da7fcf9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
