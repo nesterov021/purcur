79486a5ea58fef21bc2a586a9449b798d3c267e8
nesterov021 <61201519+nesterov021@users.noreply.github.com>
2020-06-18 12:36:10 +0500

    Updates

--- precache-manifest.37ee186071d0714e01582076347253ec.js
+++ precache-manifest.37ee186071d0714e01582076347253ec.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d2e8c72754e81b31f840b8eb4d56ecfa",
+    "revision": "c38bf276ce430416085b160833105c5f",
     "url": "./index.html"
   },
   {
-    "revision": "2e32ceff496da874c8f5",
+    "revision": "c1d5b536cdc9876570aa",
     "url": "./static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "57853e507ec86218da89",
+    "revision": "0c5fc21a3c3522fbb3fc",
     "url": "./static/css/main.981f7211.chunk.css"
   },
   {
-    "revision": "2e32ceff496da874c8f5",
-    "url": "./static/js/2.1a40872b.chunk.js"
+    "revision": "c1d5b536cdc9876570aa",
+    "url": "./static/js/2.63ff550c.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "./static/js/2.1a40872b.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.63ff550c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "57853e507ec86218da89",
-    "url": "./static/js/main.8a28c0fb.chunk.js"
+    "revision": "0c5fc21a3c3522fbb3fc",
+    "url": "./static/js/main.ce88967e.chunk.js"
   },
   {
     "revision": "cfe92b999198c276e34d",
